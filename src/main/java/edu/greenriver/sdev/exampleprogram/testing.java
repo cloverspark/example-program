188b036c076c2be93ab234baccf0fac4f97550f9
cloverspark <alexbrenna1999@hotmail.com>
2023-09-29 09:37:05 -0700

    testing adding file

---
+++ src/main/java/edu/greenriver/sdev/exampleprogram/testing.java
@@ -0,0 +1,17 @@
+package edu.greenriver.sdev.exampleprogram;
+
+public class testing {
+    private String test;
+
+    testing() {
+        this.setTest("yes");
+    }
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+}
