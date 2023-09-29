a5a275edc6a46d2137fc6061047099b060d71fdb
cloverspark <alexbrenna1999@hotmail.com>
2023-09-29 09:30:01 -0700

    Initial commit

---
+++ src/main/java/edu/greenriver/sdev/exampleprogram/ExampleProgramApplication.java
@@ -0,0 +1,20 @@
+package edu.greenriver.sdev.exampleprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Alex brenna
+ * @version 1
+ */
+@SpringBootApplication
+public class ExampleProgramApplication {
+
+	/**
+	 * @param args command args not used
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleProgramApplication.class, args);
+	}
+
+}
